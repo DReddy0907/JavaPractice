5ff6bfbcbbb2e69eb7a18e06708b5d661064ff8f
DReddy0907 <157252759+DReddy0907@users.noreply.github.com>
2024-01-20 20:41:57 -0600

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,12 @@
+public class Add {
+    public static  void main(String[] args) {
+        int a= 70;
+        int b;
+        b= 40;
+        System.out.println("The sum of two numbers is "+(a+b));
+        System.out.println("The Difference of two numbers is "+(a-b));
+        System.out.println("The Multiplication of two numbers is "+(a*b));
+        System.out.println("The Division of two numbers is "+(a/b));
+
+    }
+    }
